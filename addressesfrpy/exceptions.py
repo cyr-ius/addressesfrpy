b9db81f66cae6978d1d50493cc199d16b6c15090
cyr-ius <cyr-ius@ipocus.net>
2025-08-16 13:02:15 +0200

    Update

--- addressesfrpy/exceptions.py
+++ addressesfrpy/exceptions.py
@@ -1,5 +1,6 @@
 class AddressFrException(Exception):
     """Base exception for AddressFrpy errors."""
 
+
 class AddressNotFound(AddressFrException):
     """Exception raised when an address is not found."""
